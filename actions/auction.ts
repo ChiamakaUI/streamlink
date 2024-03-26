4e1677a4934d256c36f5d4564a10572788fdcd02
ChiamakaUI <ezembachiamaka@gmail.com>
2024-03-26 17:00:51 +0100

    cron auction

--- actions/auction.ts
+++ actions/auction.ts
@@ -1,9 +1,21 @@
 "use server";
 
-import { startCron } from "@/cron";
+// import { startCron } from "@/cron";
+import { getProductsByAuction } from "./product";
+import { getAuctionBids } from "./bids";
 
 export const startAuction = async (streamId: string) => {
- console.log("hey auction")
- 
-    startCron(streamId)
-}
+  console.log("hey auction");
+
+  // startCron(streamId)
+  const products = await getProductsByAuction(streamId);
+  for (const product of products) {
+    await new Promise((resolve) => {
+      setTimeout(async () => {
+        console.log(product); // Do something with item
+        await getAuctionBids(product.id);
+        resolve(undefined);
+      }, 3000); // Adjust the delay time as needed
+    });
+  }
+};
--- cron.ts
+++ cron.ts
@@ -1,15 +1,24 @@
 import cron from "node-cron";
-import { getProductsByAuction } from "@/actions/product";
+import { getProductsByAuction, getCurrentProduct } from "@/actions/product";
 import { getAuctionBids } from "@/actions/bids";
+// import { io } from "socket.io-client";
+
+// const socket = io("http://localhost:3000");
 
 export const startCron = async (streamId: string) => {
   console.log("hey cron");
 
   const products = await getProductsByAuction(streamId);
   products.forEach((product) => {
+    //update started at
     cron.schedule("*/3 * * * *", async () => {
+      // socket.emit("products", product);
       await getAuctionBids(product.id);
+      // await getCurrentProduct(product.id)
     });
+        //update ended at in setimeout 
+        // emit websocket to get product in settimeout
+
   });
 };
 
