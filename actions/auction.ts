d81b0a2734587eaaa719d9ad20872e6b120d3748
ChiamakaUI <ezembachiamaka@gmail.com>
2024-03-26 23:54:20 +0100

    schema changes

--- actions/auction.ts
+++ actions/auction.ts
@@ -7,13 +7,29 @@ import { getAuctionBids } from "./bids";
 export const startAuction = async (streamId: string) => {
   console.log("hey auction");
 
-  // startCron(streamId)
   const products = await getProductsByAuction(streamId);
+  
   for (const product of products) {
+    await db.product.update({
+      where: {
+        id: product.id,
+      },
+      data: {
+        auctionStarted: Date.now(),
+      },
+    });
     await new Promise((resolve) => {
       setTimeout(async () => {
         console.log(product); // Do something with item
         await getAuctionBids(product.id);
+        await db.product.update({
+          where: {
+            id: product.id,
+          },
+          data: {
+            auctionEnded: Date.now(),
+          },
+        });
         resolve(undefined);
       }, 3000); // Adjust the delay time as needed
     });
